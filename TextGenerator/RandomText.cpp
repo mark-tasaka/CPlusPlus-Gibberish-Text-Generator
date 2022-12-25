f2b8133ea894fcaa995d25582a2ff78c6ce48783
Mark Tasaka <mark.tasaka@gmail.com>
2022-12-24 21:59:56 -0800

    Initial commit

---
+++ TextGenerator/RandomText.cpp
@@ -0,0 +1,4 @@
+#include<iostream>
+#include<string>
+#include"RandomText.h"
+using namespace std;
---
+++ TextGenerator/RandomText.h
@@ -0,0 +1,14 @@
+#ifndef RANDOM_TEXT_H
+#define RANDOM_TEXT_H
+#include<iostream>
+#include<string>
+using namespace std;
+
+class RandomText {
+	public:
+
+	private:
+};
+
+#endif // !RANDOM_TEXT_H
+
